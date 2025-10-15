08245552678355676f180432c67d21caec0e6eb8
nguyentuannd080791 <nguyentuannd080791@gmail.com>
2025-10-15 08:50:54 +0700

    Revert change
    Revert change
    Reason: conflict URL when commit

--- Project_SWP391/src/java/model/Inventory_transactions.java
+++ Project_SWP391/src/java/model/Inventory_transactions.java
@@ -6,18 +6,13 @@ public class Inventory_transactions {
 
     private int tx_id;
     private String tx_type;
-    private int product_id, unit_id, qty;
-    private Integer from_location, to_location;
+    private int product_id, unit_id, qty, from_location, to_location;
     private String ref_code;
-    private Integer related_inbound_id, related_outbound_id;
-    private int employee_id;
+    private int related_inbound_id, related_outbound_id, employee_id;
     private Date tx_date;
     private String note;
 
-    public Inventory_transactions() {
-    }
-
-    public Inventory_transactions(int tx_id, String tx_type, int product_id, int unit_id, int qty, Integer from_location, Integer to_location, String ref_code, Integer related_inbound_id, Integer related_outbound_id, int employee_id, Date tx_date, String note) {
+    public Inventory_transactions(int tx_id, String tx_type, int product_id, int unit_id, int qty, int from_location, int to_location, String ref_code, int related_inbound_id, int related_outbound_id, int employee_id, Date tx_date, String note) {
         this.tx_id = tx_id;
         this.tx_type = tx_type;
         this.product_id = product_id;
@@ -31,6 +26,9 @@ public class Inventory_transactions {
         this.employee_id = employee_id;
         this.tx_date = tx_date;
         this.note = note;
+    }
+
+    public Inventory_transactions() {
     }
 
     public int getTx_id() {
@@ -73,19 +71,19 @@ public class Inventory_transactions {
         this.qty = qty;
     }
 
-    public Integer getFrom_location() {
+    public int getFrom_location() {
         return from_location;
     }
 
-    public void setFrom_location(Integer from_location) {
+    public void setFrom_location(int from_location) {
         this.from_location = from_location;
     }
 
-    public Integer getTo_location() {
+    public int getTo_location() {
         return to_location;
     }
 
-    public void setTo_location(Integer to_location) {
+    public void setTo_location(int to_location) {
         this.to_location = to_location;
     }
 
@@ -97,19 +95,19 @@ public class Inventory_transactions {
         this.ref_code = ref_code;
     }
 
-    public Integer getRelated_inbound_id() {
+    public int getRelated_inbound_id() {
         return related_inbound_id;
     }
 
-    public void setRelated_inbound_id(Integer related_inbound_id) {
+    public void setRelated_inbound_id(int related_inbound_id) {
         this.related_inbound_id = related_inbound_id;
     }
 
-    public Integer getRelated_outbound_id() {
+    public int getRelated_outbound_id() {
         return related_outbound_id;
     }
 
-    public void setRelated_outbound_id(Integer related_outbound_id) {
+    public void setRelated_outbound_id(int related_outbound_id) {
         this.related_outbound_id = related_outbound_id;
     }
 
